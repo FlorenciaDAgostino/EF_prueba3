8f72796ed63dff4121f5a94577abbe8c98905272
Florencia DAgostino <dagostinoflorencia@gmail.com>
2025-07-20 16:54:17 -0300

    cambios última clase

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import UserRegisterView, CustomLoginView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
     
     
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView, UpdateView, DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 from .forms import RegisterForm
 
@@ -12,4 +13,18 @@ class UserRegisterView(CreateView):
     model = User
     form_class = RegisterForm
     template_name = 'usuarios/register.html'
+    success_url = reverse_lazy('login')
  
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = authenticate(
+            username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
+        if user:
+            login(self.request, user)
+        return response
+    
+class CustomLoginView(LoginView):
+    template_name = 'usuarios/login.html' 
+
+    def get_success_url(self):
+        return reverse_lazy('inicio')
